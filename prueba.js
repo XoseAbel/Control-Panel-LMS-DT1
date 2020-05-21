92deec787f1babb9ca2b67ff644785d2ceaab2eb
Suso <susopc1981@hotmail.com>
2020-05-21 11:41:54 +0200

    Paxina carrito lista a falta da navegacion esquerda

--- prueba.js
+++ prueba.js
@@ -4,17 +4,13 @@ function muestra_oculta(id) {
     var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
     el.style.display = el.style.display == "none" ? "block" : "none"; //damos un atributo display:none que oculta el div
   }
-  console.log(el);
 }
 
 function mostrarnoarticles(id) {
-  if (document.getElementById) var juan = document.getElementById(id);
+  if (document.getElementById) var numarticles = document.getElementById(id);
   if (count_click > 1) {
-    juan.style.display = "none";
-    console.log("no hay");
+    numarticles.style.display = "none";
   } else {
-    juan.style.display = "block";
-    console.log("hay");
+    numarticles.style.display = "block";
   }
-  console.log(juan);
 }
